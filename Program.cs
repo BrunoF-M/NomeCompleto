62348c53a394ff98179543b95f2e22fe76b30e60
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-11 20:23:33 -0300

    aula completa dia 11

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula_entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDoUsuario;
+            string SobreNome;
+            string nomeCompleto;
+
+            Console.Write("Por favor, digite seu nome: ");
+            nomeDoUsuario = Console.ReadLine();
+            
+            Console.Write("agora seu sobrenome: ");
+            SobreNome = Console.ReadLine();
+
+            nomeCompleto = $"{nomeDoUsuario} {SobreNome}";
+            
+            Console.WriteLine($"Obrigado, {nomeCompleto.ToUpper()}! Presione uma tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
